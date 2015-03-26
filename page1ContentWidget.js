89683a38cde5b8ea169bcaac722cd159e1a164ef
Pooja Shyam <pooja.shyam@photoninfotech.net>
2015-03-26 14:03:10 -0500

    commit

---
+++ page1ContentWidget.js
@@ -0,0 +1,29 @@
+Clazz.page1ContentWidget = Clazz.extend(
+    Clazz.WidgetWithTemplate,{
+        initialize : function(config){
+            this.templateUrl = "page1Content.html";
+        },
+        bindUI: function() {
+            var y = function() {
+                document.Navigate("page2")
+            };
+            $("#forwardButton").click(y);
+       },
+        jq1: function()
+        {
+            $(document).ready(function(){
+                $("#serverConnection").click(function(e){
+                    e.placeData();
+                    $.ajax({
+                        url: "http://localhost:3000/details",
+                        type: "POST",
+                //data: postJson,
+                        success: function(obj) {
+                            $("#result").html(obj);
+                        },
+                    });
+                });
+            });
+        }
+    }
+);
